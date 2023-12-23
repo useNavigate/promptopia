9b5b5060552a30af18570bac28c442a944162d9b
useNavigate <useNavigate@gmail.com>
2023-12-23 15:33:49 -0500

    set up googlt oAuth

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,13 +10,15 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"/>
-        </div>
-        <main className="app">
-          <Nav/>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
             {children}
-        </main>
+          </main>
+        </Provider>
       </body>
     </html>
   );
