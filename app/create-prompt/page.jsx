03a39891e3b767314f6c7a39a1bb22b0ec9f0f30
useNavigate <useNavigate@gmail.com>
2023-12-23 20:10:04 -0500

    working on form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
+import {useSession} from "next-auth/react"
+import {useRouter} from "next/navigation"
+import Form from '../../components/Form'
 
 const CreatePrompt = () => {
+   const [submitting, setSubmitting] = useState(false);
+   const [post, setPost] = useState({
+     prompt: "",
+     tag: "",
+   });
+   const createPrompt = async (e) => {
+     e.preventDefault();
+     setSubmitting(true);
+   };
   return (
-    <div>CreatePrompt</div>
-  )
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 }
 
 export default CreatePrompt
