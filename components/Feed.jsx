8e24e148213db0fd8fc318bcdd797b79b772b393
useNavigate <useNavigate@gmail.com>
2023-12-26 14:35:46 -0500

    search feature has been added

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,55 +1,95 @@
-'use client'
-import React,{useState,useEffect} from 'react'
-import PromptCard from "./PromptCard"
+"use client";
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
-const PromptCardList =({data,handleTagClick})=>{
-  return(
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
     <div className="mt-16 prompt_layout">
-      {data.map((post)=>{
-        return(
-          <PromptCard key={post.id} post={post} handleTagClick={handleTagClick}/>
-        )
+      {data.map((post) => {
+        return (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        );
       })}
     </div>
-  )
-}
-
-
+  );
+};
 
 const Feed = () => {
-  const [searchText,setSearchText] = useState("")
-  const [posts,setPosts] = useState([])
-  const handleSearchChange = (e) => {
+  //search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchResults, setSearchResults] = useState([]);
 
+  const [posts, setPosts] = useState([]);
+
+
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const filterPrompts=(searchText)=>{
+    const regex = new RegExp(searchText, "i");
+    //i flag for case insensitive search
+
+    return posts.filter((item)=> regex.test(item.prompt)|| regex.test(item.tag)|| regex.test(item.creator.username))
   }
 
-  useEffect(()=>{
-    const fetchPosts = async()=>{
-      const response = await fetch("/api/prompt")
-      const data = await response.json()
-      setPosts(data)
-    }
-    fetchPosts()
-  },[])
+  const handleSearchChange=(e)=>{
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    //debounce method
+
+    setSearchTimeout(
+      setTimeout(() => {
+       const searchResult = filterPrompts(e.target.value);
+       setSearchResults(searchResult);
+      }, 500)
+    );
+  }
+
+
+ const handleTagClick = (tagName) => {
+  setSearchText(tagName);
+
+  const searchResult = filterPrompts(tagName);
+  setSearchResults(searchResult);
+ }
+
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input
-        text="text"
-        placeholder="Search for a tag or username"
-        value={searchText}
-        onChange={handleSearchChange}
-        required
-        className="search_input peer"
+          text="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
         />
-
       </form>
-      <PromptCardList
-
-      data={posts}
-      handleTagClick={()=>{}}/>
+      {searchText?(
+        <PromptCardList
+          data={searchResults}
+          handleTagClick={handleTagClick}
+        />
+      ):(
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
